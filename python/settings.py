4129f6c6514c19cb6539d715848fe443af445f46
LuongLuyen <luongvanluyen2003@gmail.com>
2023-11-02 12:13:07 +0700

    setup

--- python/settings.py
+++ python/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'web',
         'USER': 'root',
-        'PASSWORD': 'luyen123',
+        'PASSWORD': '',
         'HOST': 'localhost',  
         'PORT': '3306',   
     }
